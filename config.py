42d1b0e5dcbb02028b05ed76d72c09769187c1e2
Yuuichiexe <137192221+Yuuichiexe@users.noreply.github.com>
2025-03-17 13:21:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -78,13 +78,13 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://te.legra.ph/file/f196c57731dc9e2ea6886.jpg"
+    "START_IMG_URL", "https://files.catbox.moe/zt9voh.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://te.legra.ph/file/f196c57731dc9e2ea6886.jpg"
+    "PING_IMG_URL", "https://files.catbox.moe/zt9voh.jpg"
 )
 PLAYLIST_IMG_URL = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
-STATS_IMG_URL = "https://te.legra.ph/file/e906c2def5afe8a9b9120.jpg"
+STATS_IMG_URL = "https://files.catbox.moe/zt9voh.jpg"
 TELEGRAM_AUDIO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
 TELEGRAM_VIDEO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
 STREAM_IMG_URL = "https://te.legra.ph/file/bd995b032b6bd263e2cc9.jpg"
